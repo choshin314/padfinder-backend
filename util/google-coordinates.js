d325489ac77374c42331eeddcc822343bba75f95
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-03 01:23:49 -0500

    swapped in axios in getCoordinates (forgot fetch doesnt work)

--- util/google-coordinates.js
+++ util/google-coordinates.js
@@ -1,13 +1,13 @@
 const HttpError = require('../models/http-error')
+const axios = require('axios').default;
 
 const getCoordinates = async (address, next) => {
     let coordinates;
     try {
-        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAPS_KEY}`);
-        const data = await response.json();
-        switch (data.status) {
+        const responseData = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAPS_KEY}`);
+        switch (responseData.status) {
             case "OK": 
-                coordinates = data.results[0].geometry.location; 
+                coordinates = responseData.results[0].geometry.location; 
                 break;
             case "ZERO_RESULTS": 
                 throw new HttpError('Address is invalid.  Please submit a valid property address.', 400);
