18efe471c82ec5f6e582c691952e9ec4067cfa50
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-13 23:14:16 -0500

    add formatted_address to getCoordinates return values

--- util/google-coordinates.js
+++ util/google-coordinates.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 const getCoordinates = async (address, next) => {
     let coordinates;
+    let formatted_address;
     try {
         const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAPS_KEY}`);
         const responseData = response.data;
         switch (responseData.status) {
             case "OK": 
                 coordinates = responseData.results[0].geometry.location; 
+                formatted_address = responseData.results[0].formatted_address;
                 break;
             case "ZERO_RESULTS": 
                 throw new HttpError('Address is invalid.  Please submit a valid property address.', 400);
@@ -26,8 +28,7 @@ const getCoordinates = async (address, next) => {
     } catch (err) {
         return next(err)
     }
-    console.log(coordinates)
-    return coordinates;
+    return { coordinates, formatted_address }
 }
 
 module.exports = getCoordinates;
