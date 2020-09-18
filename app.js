d111ddffbedaada7405d813858531dd95a2fa33b
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-18 14:59:10 -0500

    passed in options to mongoose connect to get rid of deprecation warnings

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/user-routes')
 
 //------------DB-------------------------
 const dbUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fvtaz.mongodb.net/padfinder?retryWrites=true&w=majority`
-mongoose.connect(dbUri)
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  })
     .then(() => console.log('Connected to MongoDB'))
 
 //------------MIDDLEWARE------------------
